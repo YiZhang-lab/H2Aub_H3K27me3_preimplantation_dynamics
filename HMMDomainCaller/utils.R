de44f22109626754adeeedcf7a5fbaa5f0b6bfb8
sirusb <djek.nad@gmail.com>
2023-04-21 05:57:56 -0500

    Update utils.R

--- HMMDomainCaller/utils.R
+++ HMMDomainCaller/utils.R
@@ -389,7 +389,8 @@ CallDomains <- function(bw_file,
   refGenome = switch (genome,
                       mm9 = BSgenome.Mmusculus.UCSC.mm9::Mmusculus,
                       mm10 = BSgenome.Mmusculus.UCSC.mm10::Mmusculus,
-                      hg19 = BSgenome.Mmusculus.UCSC.hg19::BSgenome.Mmusculus.UCSC.hg19
+                      hg19 = BSgenome.Hsapiens.UCSC.hg19::Hsapiens,
+                      hg38 = BSgenome.Hsapiens.UCSC.hg38::Hsapiens
   )
   
   cat("\n")
